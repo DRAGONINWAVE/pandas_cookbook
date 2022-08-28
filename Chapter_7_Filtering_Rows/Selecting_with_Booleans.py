479e1b4ad94974c40cb16d1ac4d717df109248cd
DRAGONINWAVE <yl597591@gmail.com>
2022-08-28 09:38:09 +0800

    pandas foundations

--- Chapter_7_Filtering_Rows/Selecting_with_Booleans.py
+++ Chapter_7_Filtering_Rows/Selecting_with_Booleans.py
@@ -1,4 +1,6 @@
+from calendar import prcal
 import pandas as pd
+import numpy as np
 
 movie = pd.read_csv(
 	r'D:\python3.10\Pandas-Cookbook-master\data\movie.csv',
@@ -10,3 +12,25 @@ criteria = c1 & c2
 movie_loc = movie.loc[criteria]
 print(movie_loc.head())
 print(movie_loc.equals(movie[criteria]))
+movie_iloc = movie.iloc[criteria.to_numpy()]
+print(
+	movie_iloc.equals(movie_loc))
+
+criteria_col  =movie.dtypes == np.int64
+print(
+	criteria_col.head())
+print(movie.loc[:,criteria_col].head())
+print(movie.iloc[:,criteria_col.to_numpy()].head())
+cols = [
+	'content_rating',
+	'imdb_score',
+	'title_year',
+	'gross',
+	]
+print(movie.loc[criteria,cols].sort_values('imdb_score'))
+col_index = [movie.columns.get_loc(col) for col in cols]
+print(col_index)
+a = criteria.to_numpy()
+print(a[:5])
+print(len(a),len(criteria))
+print(movie.select_dtypes(int))
