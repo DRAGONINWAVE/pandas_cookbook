c83bf59a3408a0b735c2c05987d576dfc0bc725e
DRAGONINWAVE <yl597591@gmail.com>
2022-09-01 13:07:53 +0800

    foundations_pandas

--- Chapter_11_Combining_Pandas_Objects/Connectiong_to_SQL_databases.py
+++ Chapter_11_Combining_Pandas_Objects/Connectiong_to_SQL_databases.py
@@ -2,5 +2,6 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 engine = create_engine('sqlite:///data/chinook.db')
+
 tracks = pd.read_sql_table('tracks',engine)
-print()
+print(tracks.head())
